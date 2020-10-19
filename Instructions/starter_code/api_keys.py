dcd2c1dc34babd21638d3209f76f75006dccfb44
saronraut <47161225+saronraut@users.noreply.github.com>
2020-10-18 22:58:52 -0400

    finally got the api to work

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bfea2649ace398c087e0327c33a4633c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBdkQP7bqryY0Zn7edG4CPHHmQzonk2Yrk"
