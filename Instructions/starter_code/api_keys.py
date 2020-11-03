6495100062f8d2e3d3a68732cb0e5952d5746a18
saronraut <47161225+saronraut@users.noreply.github.com>
2020-11-02 22:12:40 -0500

    completed api assignment

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bfea2649ace398c087e0327c33a4633c"
+weather_api_key = "INSERT YOUR KEYS HERE"
 
 # Google API Key
-g_key = "AIzaSyBdkQP7bqryY0Zn7edG4CPHHmQzonk2Yrk"
+g_key = "INSERT YOUR KEYS HERE"
